7fb8a8b024572cdb6cb93e08c6785858e24b290f
unknown <song.gul94@gmail.com>
2023-10-19 14:58:49 +0300

    Updates

--- assets/PageNotFound-688e3e9f.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-d1ada107.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
---
+++ assets/PageNotFound-8b3f3f55.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-3990ac07.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
