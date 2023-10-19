ba1fdf4e0a6ceca00e3ca936b9d0f1d51ae8e65a
unknown <song.gul94@gmail.com>
2023-10-19 14:39:36 +0300

    added base vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    base: "/",
+    plugins: [react(), eslint()],
+  };
+  if (command !== "serve") {
+    config.base = "/vite-worldpackers-app/";
+  }
+  return config;
 });
